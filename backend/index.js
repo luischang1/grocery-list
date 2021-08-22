eb601ef4079895cfca32952e197874443020b9c9
luischang1 <luischang1@gmail.com>
2021-08-22 11:51:36 -0500

    mocked products API

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const fs = require('fs')
+
+app.get('/', (req, res) => {
+  console.log('/');
+  res.end('Hello World!');
+});
+
+app.get("/products", (req, res) => {
+  console.log('/products');
+  fs.readFile(__dirname + '/mocks/products.json', 'utf8', (err, data) => {
+    res.end(data);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`)
+});
